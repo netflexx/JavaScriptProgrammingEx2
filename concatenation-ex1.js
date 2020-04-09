689f81ec573e5287bd2e5a59023283f0eeea7e93
QubitCupid <vectorvuqubit@gmail.com>
2020-04-08 22:07:57 -0300

    ex2

---
+++ concatenation-ex1.js
@@ -0,0 +1,10 @@
+let text = `Web and Mobile Application development was the program Fred entered in the Fall of 2019.
+            Intro to Programming is going to be the course Fred likes best. Fred is going to study very hard.
+            Each night Fred is going to spend time reviewing all of the course material.
+            Fred will be committed to learning as much as possible about C# in 2019!`
+let fred = "Fred";
+
+console.log(`Web and Mobile Application development was the program ${fred} entered in the Fall of 2019.
+Intro to Programming is going to be the course ${fred} likes best. Fred is going to study very hard.
+Each night ${fred} is going to spend time reviewing all of the course material.
+${fred} will be committed to learning as much as possible about C# in 2019!`)
---
+++ concatenation-ex2.js
@@ -0,0 +1,5 @@
+let firstName = "Duc", lastName = "Vu", age = 20, city = "hk", married = "false", job = "wb", rate = 10;
+
+console.log(`Hello, ${firstName} ${lastName}. You are ${age} years old and were born in ${city}, and currently live in Moncton.
+Your first job was at ${job} making ${rate}/hour.
+I asked if you were married and you said ${married}.`)
